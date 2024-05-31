dc93b1251edf55bb0ace7edf68d15ff67592090a
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2024-05-31 01:31:46 -0500

    refactored

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import os
-import sys
-import cv2
-import traceback
-from utils.file_processing import load_frames
+import os, sys, traceback
 from invariant_features import ImageStitcher
-
-def list_files(directory):
-    file_directories = []
-    for file in sorted(os.listdir(directory)):
-        file_directories.append(directory+file)
-    return file_directories
+from utils.file_processing import load_frames, list_files, save_image
 
 def stitch_image(file_directories: list, output_dir: str, skip_size = 5):
     stitcher = ImageStitcher()
@@ -17,8 +8,8 @@ def stitch_image(file_directories: list, output_dir: str, skip_size = 5):
         print(f"\033[94m[Log]\033[0m Stitching image {idx+1} of {int(len(file_directories)/skip_size)} images.")
         stitcher.add_image(frame)
         stitched_image = stitcher.image()
-        cv2.imwrite(output_dir+str(idx)+".png", stitched_image)
-    cv2.imwrite(output_dir+"0_final_image.png", stitched_image)
+        save_image(output_dir+str(idx)+".png", stitched_image)
+    save_image(output_dir+"0_final_image.png", stitched_image)
 
 if __name__ == "__main__":
     try:
--- utils/file_processing.py
+++ utils/file_processing.py
@@ -1,10 +1,6 @@
-import cv2
-import numpy
-import pathlib
-from typing import List
-from typing import Generator
+import os, cv2, numpy, pathlib
+from typing import List, Generator
 from matplotlib import pyplot as plt
-
 
 def load_frames(paths: List[str]) -> Generator[numpy.ndarray, None, None]:
     for path in paths:
@@ -14,15 +10,21 @@ def load_frames(paths: List[str]) -> Generator[numpy.ndarray, None, None]:
         elif path.suffix.lower() in ['.jpg', '.jpeg', '.png']:
             yield cv2.imread(str(path))
 
-
 def load_images(image_paths):
     images = []
     for path in image_paths:
         images.append(cv2.imread(path))
     return images
 
-def save_image(image, path):
+def list_files(directory):
+    file_directories = []
+    for file in sorted(os.listdir(directory)):
+        file_directories.append(directory+file)
+    return file_directories
+
+def save_image(path, image):
     cv2.imwrite(path, image)
+    print(f"\033[94m[Log]\033[0m Final image saved at \033[94m{path}\033[0m")
 
 def show_image(image):
     plt.imshow(image)
